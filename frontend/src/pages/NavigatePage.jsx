0c8a801d9b684a01cbadb2cfa8e8b477fd186310
Vihangi Rathnayake <89300272+IT21271182@users.noreply.github.com>
2024-05-07 13:21:59 +0530

    Added panda

--- frontend/src/pages/NavigatePage.jsx
+++ frontend/src/pages/NavigatePage.jsx
@@ -3,12 +3,13 @@ import { Button } from "flowbite-react";
 
 export default function NavigatePage() {
 return(
-    <>
+    <div>
+    <img src="/edupanda.png" className="h-60 w-auto"/>
 
     <Button gradientDuoTone="purpleToBlue" className="p-2 m-4">Admin</Button>
 
     <Button gradientDuoTone="purpleToBlue" className="p-2 m-4">Instructor</Button>
 
-    </>
+    </div>
 )
 }
