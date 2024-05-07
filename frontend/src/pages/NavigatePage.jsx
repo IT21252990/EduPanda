87174ab195ba315aa867889f294726b7037206f0
Vihangi Rathnayake <89300272+IT21271182@users.noreply.github.com>
2024-05-07 14:15:52 +0530

    Nav bar

--- frontend/src/pages/NavigatePage.jsx
+++ frontend/src/pages/NavigatePage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Button } from "flowbite-react";
 
+
 export default function NavigatePage() {
 return(
     <div>
     <img src="/edupanda.png" className="h-60 w-auto"/>
 
-    <Button gradientDuoTone="purpleToBlue" className="p-2 m-4">Admin</Button>
+    <Button gradientDuoTone="purpleToBlue" className="p-2 m-4" href="/AdminHome">Admin</Button>
 
     <Button gradientDuoTone="purpleToBlue" className="p-2 m-4">Instructor</Button>
 
