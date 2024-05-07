23c72b200a50d2baf9aeb85494484c32953df0d9
Kalinga Jayathilaka <kalingajayathilaka@gmail.com>
2024-05-07 14:47:37 +0530

    cerate instructor home

---
+++ frontend/src/pages/instructor/InstructorHome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function InstructorHome() {
+  return (
+    <div className='flex justify-center items-center w-screen h-full'>
+      <h1>InstructorHome</h1>
+    </div>
+  )
+}
+
+export default InstructorHome
