9aa0d3f1f915583e0added1c09a9a0f5241c94ed
Vihangi Rathnayake <89300272+IT21271182@users.noreply.github.com>
2024-05-07 20:47:56 +0530

    nav bar

--- frontend/src/components/admin/AdSideBar.jsx
+++ frontend/src/components/admin/AdSideBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
-export default function SidebarAndNavbar() {
+export default function AdSidebar() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const toggleDropdown = () => {
@@ -10,61 +10,7 @@ export default function SidebarAndNavbar() {
   };
 
   return (
-    <>
-      <nav className="fixed top-0 left-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-        <div className="px-3 py-2.5 lg:px-5 lg:pl-3">
-          <div className="flex items-center justify-between">
-            <div className="flex items-center justify-start rtl:justify-end">
-              <button
-                data-drawer-target="logo-sidebar"
-                data-drawer-toggle="logo-sidebar"
-                aria-controls="logo-sidebar"
-                type="button"
-                className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              >
-                <span className="sr-only">Open sidebar</span>
-                <svg
-                  className="w-6 h-6"
-                  aria-hidden="true"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    clip-rule="evenodd"
-                    fill-rule="evenodd"
-                    d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-                  ></path>
-                </svg>
-              </button>
-              <a href="#" className="flex ms-2 md:me-24">
-                <img src="/edupanda.png" className="h-9 me-5" alt="Logo" />
-                <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
-                  EduPanda
-                </span>
-              </a>
-            </div>
-            <div className="flex items-center">
-              <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                <div className="block mx-14 my-auto text-xl font-bold text-gray-900 md:p-0 dark:text-white ">
-                  Hello Admin !
-                </div>
-                <Link
-                  // onClick={() => {
-                  //   doSignOut().then(() => {
-                  //     navigate("/");
-                  //   });
-                  // }}
-                  className="text-white bg-blue-700 hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Log Out
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
-
+    <div>
       <aside
         id="logo-sidebar"
         className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
@@ -74,7 +20,7 @@ export default function SidebarAndNavbar() {
           <ul className="space-y-2 font-medium">
             <li>
               <a
-                href="#"
+                href="/AdminHome"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <svg
@@ -140,7 +86,7 @@ export default function SidebarAndNavbar() {
               >
                 <li>
                   <a
-                    href="#"
+                    href="/ViewAllCourses"
                     className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                   >
                     All Courses
@@ -148,7 +94,7 @@ export default function SidebarAndNavbar() {
                 </li>
                 <li>
                   <a
-                    href="#"
+                    href="/NewCourses"
                     className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                   >
                     New Courses
@@ -156,7 +102,7 @@ export default function SidebarAndNavbar() {
                 </li>
                 <li>
                   <a
-                    href="#"
+                    href="/UpdatedCourses"
                     className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                   >
                     Updated Courses
@@ -239,6 +185,6 @@ export default function SidebarAndNavbar() {
           </ul>
         </div>
       </aside>
-    </>
+    </div>
   );
 }
