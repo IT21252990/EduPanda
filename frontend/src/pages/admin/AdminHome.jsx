38d221646205f747a59e35658916f563c0ee4c02
Vihangi Rathnayake <89300272+IT21271182@users.noreply.github.com>
2024-05-07 14:52:36 +0530

    Update AdminHome.jsx

--- frontend/src/pages/admin/AdminHome.jsx
+++ frontend/src/pages/admin/AdminHome.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Nav from "../../components/admin/Nav";
 import SidebarAndNavbar from "../../components/admin/SidebarAndNavbar";
 
 export default function AdminHome() {
