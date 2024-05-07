f72c052cd02573b5659f91e2c4a40b6649f3fc3a
Vihangi Rathnayake <89300272+IT21271182@users.noreply.github.com>
2024-05-07 14:50:22 +0530

    Side bar and nav bar

--- frontend/src/pages/admin/AdminHome.jsx
+++ frontend/src/pages/admin/AdminHome.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Nav from "../../components/admin/Nav";
+import SidebarAndNavbar from "../../components/admin/SidebarAndNavbar";
 
 export default function AdminHome() {
 return(
     <>
-    <Nav />
+    < SidebarAndNavbar />
 
     </>
 )
