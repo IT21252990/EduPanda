3ce78781731f1c0bfc716d7b902f3ab020aa5778
Vihangi Rathnayake <89300272+IT21271182@users.noreply.github.com>
2024-05-08 22:26:23 +0530

    Course backend done

--- backend/admin-service/src/routes/courseRoutes.js
+++ backend/admin-service/src/routes/courseRoutes.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const {
-    getAllCourses
+    getAllCourses,
+    getNewCourses,
+    getUpdatedCourses,
+    approveCourse,
+    rejectCourse
 } = require("../controllers/courseControllers");
 
 const router = express.Router();
 
-// GET all Courses
+// GET all courses
 router.get("/", getAllCourses);
+// GET new courses
+router.get("/new", getNewCourses); 
+//GET updated courses
+router.get("/updated", getUpdatedCourses); 
+//Patch approve a course
+router.patch("/approve/:id", approveCourse); 
+//Patch reject a course
+router.patch("/reject/:id", rejectCourse); 
+
 
 module.exports = router;
