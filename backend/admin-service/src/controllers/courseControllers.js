0794a1ddebb6723a9d80ac09fca26364feede1c2
Vihangi Rathnayake <89300272+IT21271182@users.noreply.github.com>
2024-05-08 22:26:23 +0530

    Course backend done

--- backend/admin-service/src/controllers/courseControllers.js
+++ backend/admin-service/src/controllers/courseControllers.js
@@ -13,6 +13,77 @@ const getAllCourses = async (req, res) => {
     }
 };
 
+
+//@desc Retrieve all new courses with update: false and status: pending
+//@route GET /api/courses/new
+//@access public
+const getNewCourses = async (req, res) => {
+    try {
+        const newCourses = await Course.find({ status: 'Pending', Updated: false });
+        res.json(newCourses);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+//@desc Retrieve all updated courses with update: true and status: pending
+//@route GET /api/courses/updated
+//@access public
+const getUpdatedCourses = async (req, res) => {
+    try {
+        const updatedCourses = await Course.find({ status: 'Pending', Updated: true });
+        res.json(updatedCourses);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+//@desc Approve a course
+//@route PATCH /api/courses/approve/:id
+//@access public
+const approveCourse = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const course = await Course.findById(id);
+        if (!course) {
+            return res.status(404).json({ message: "Course not found" });
+        }
+        course.status = 'Published';
+        course.Updated = false;
+        await course.save();
+        res.json({ message: "Course approved successfully", course });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+        console.log({ message: error.message });
+    }
+};
+
+//@desc Approve a course
+//@route PATCH /api/courses/approve/:id
+//@access public
+const rejectCourse = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const course = await Course.findById(id);
+        if (!course) {
+            return res.status(404).json({ message: "Course not found" });
+        }
+        course.status = 'Rejected';
+        course.Updated = false;
+        await course.save();
+        res.json({ message: "Course rejected successfully", course });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+        console.log({ message: error.message });
+    }
+};
+
+
 module.exports = {
     getAllCourses,
+    getNewCourses,
+    getUpdatedCourses,
+    approveCourse,
+    rejectCourse
 };
