6d0d3db371ffad0d25dbe7b6bfec73d6967bb742
Vihangi Rathnayake <89300272+IT21271182@users.noreply.github.com>
2024-05-10 05:38:23 +0530

    css correction

--- frontend/src/components/admin/Finance/RevenueChart.jsx
+++ frontend/src/components/admin/Finance/RevenueChart.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 export default function RevenueChart() {
   return (
-    <div class="max-w-sm w-full bg-white rounded-lg shadow dark:bg-gray-800 p-4 md:p-6">
-      <div class="flex justify-between">
+    <div className="max-w-sm w-full bg-white rounded-lg shadow dark:bg-gray-800 p-4 md:p-6">
+      <div className="flex justify-between">
         <div>
-          <h5 class="leading-none text-3xl font-bold text-gray-900 dark:text-white pb-2">
+          <h5 className="leading-none text-3xl font-bold text-gray-900 dark:text-white pb-2">
             32.4k
           </h5>
-          <p class="text-base font-normal text-gray-500 dark:text-gray-400">
+          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
             Users this week
           </p>
         </div>
