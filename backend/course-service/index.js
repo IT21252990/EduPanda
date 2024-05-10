2f343d7748c0104b01fb56199b55073e75a1d8c1
Kalinga Jayathilaka <kalingajayathilaka@gmail.com>
2024-05-10 22:55:17 +0530

    Create Dockerfiles / Docker done

--- backend/course-service/index.js
+++ backend/course-service/index.js
@@ -17,7 +17,7 @@ const courseRoutes = require('./src/routes/courseRoutes');
 
 app.use('/api/courses', courseRoutes);
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
